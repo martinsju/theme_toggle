9166def61c4668a26f63b790077594d16a55f071
julia <agramartinsj@gmail.com>
2023-04-10 19:26:24 -0300

    feat: created themes files

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+	title: 'Dark',
+
+	colors: {
+		primary: '#000',
+		secondary: '#ccc',
+
+		background: '#000',
+		text: '#fff'
+	}
+}
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+	title: 'Light',
+
+	colors: {
+		primary: '#fff',
+		secondary: '#ccc',
+
+		background: '#fff',
+		text: '#000'
+	}
+}
