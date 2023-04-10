def1e76b72a0dabd18821571592020295bc44a6a
julia <agramartinsj@gmail.com>
2023-04-10 18:56:15 -0300

    refactor: changed component to arrow function

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import Header from './components/Header'
+import GlobalStyle from './styles/global'
 
-function App() {
-	return <div>Hello</div>
+const App: React.FC = function () {
+	return (
+		<div>
+			<GlobalStyle />
+			<Header />
+		</div>
+	)
 }
 
 export default App
