4797a713208763e2c4268f49c7fffae2ce28c2f6
julia <agramartinsj@gmail.com>
2023-04-10 18:58:09 -0300

    style: global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+
+*{
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+    font-family: sans-serif;
+}
+
+body {
+    margin: 0 auto;
+    background-color: #050716;
+    color: #f7f7f7;
+}
+
+`
